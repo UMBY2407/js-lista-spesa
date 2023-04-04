7736a216a257df42dbe7f32c2a1e1fb1e3764896
UMBY2407 <umby2407@gmail.com>
2023-04-04 17:10:18 +0200

    Lista Spesa on console, using while and for

--- js/main.js
+++ js/main.js
@@ -1,24 +1,11 @@
-let listaDellaSpesa = [];
-let elementoSpesa;
-let numeroElementiSpesaStringa = prompt("Quanti Elementi vuoi comprare?");
-
-//verifica input utente
-    while(isNaN(numeroElementiSpesaStringa) || numeroElementiSpesaStringa == ""){
-        numeroElementiSpesaStringa = prompt("Hai inserito un numero non Valido! Quanti Elementi vuoi comprare?");
+let listaDellaSpesa = ["Mela", "Pera", "Arancia"];
+let i = 0;
+/*  for(let i = 0; i < listaDellaSpesa.length; i++){
+        console.log(listaDellaSpesa[i]);
     }
-    let numeroElementiSpesa = parseInt(numeroElementiSpesaStringa);
-    listaDellaSpesa = [numeroElementiSpesa - 1];
-
-//inserimento elementi spesa
-    for(let i = 0; i < numeroElementiSpesa; i++){
-        elementoSpesa = prompt("Cosa vuoi comprare?");
-
-            while(!isNaN(elementoSpesa) || elementoSpesa == ""){
-                elementoSpesa = prompt("Hai inserito un elemento non valido! Inserire un nuovo elemento:");
-            }
-        listaDellaSpesa[i] = elementoSpesa;
-        console.log(listaDellaSpesa);
-        do{
-            document.getElementById('listaSpesa').innerHTML = `<li>${listaDellaSpesa[i]}</li>`;
-        }while(i< numeroElementiSpesa);
+*/
+/*  while(listaDellaSpesa[i]){
+        console.log(listaDellaSpesa[i]);
+        i++;
     }
+*/
